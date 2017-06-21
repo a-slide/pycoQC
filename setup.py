6bca488490688bae82c05d5c5f1cbda9bc07306b
a-slide <aleg@ebi.ac.uk>
2017-06-21 15:16:08 +0100

    format setup.py

--- setup.py
+++ setup.py
@@ -17,21 +17,13 @@ from setuptools import setup
 
 setup(
     name='pycoQC',
-    
     version='1.0.dev1',
-    
     description='Python 3 package for Jupyter Notebook, computing metrics and generating simple QC plots from Oxford Nanopore technologies (ONT) Albacore basecaller',
-    
     long_description=open('README.rst', 'r').read(),
-    
     url='https://github.com/a-slide/pycoQC',
-    
     author='Adrien Leger',
-    
     author_email='aleg@ebi.ac.uk',
-    
     license='GPLv3',
-
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Science/Research',
@@ -42,14 +34,12 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',],
-       
     install_requires=[
         'numpy>=1.13.0',
         'pandas>=0.20.0',
         'matplotlib>=2.0.0',
         'seaborn>= 0.7.0',
         'notebook>=4.0.0',],
-    
     packages=["pycoQC"],
     package_dir={'pycoQC': 'pycoQC'},
     package_data={'pycoQC': ['data/sequencing_summary.txt', "test_pycoQC.ipynb"]},
