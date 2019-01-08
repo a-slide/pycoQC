148e52ac39c8f9171c13e0fd67cb332032cefa12
a-slide <aleg@ebi.ac.uk>
2019-01-08 17:48:30 +0000

    Update version number

--- pycoQC/__init__.py
+++ pycoQC/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 # Define self package variable
-__version__ = '2.0a7'
+__version__ = '2.0a8'
 __all__ = ["pycoQC", "common"]
 __description__="""
 pycoQC computes metrics and generates interactive QC plots from the sequencing
