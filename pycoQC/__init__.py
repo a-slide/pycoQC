a60f2c6025f43431833237c26650e2aeb67fb364
a-slide <aleg@ebi.ac.uk>
2019-05-03 10:35:36 +0100

    Bump version number

--- pycoQC/__init__.py
+++ pycoQC/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 # Define self package variable
-__version__ = '2.2.3.4'
+__version__ = '2.2.3.5'
 __all__ = ["pycoQC", "common"]
 __description__="""
 PycoQC computes metrics and generates interactive QC plots for Oxford Nanopore technologies sequencing data
