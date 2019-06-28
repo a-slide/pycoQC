dfe1408bce7ef1d67aba1c87b69802edc87cbb87
a-slide <aleg@ebi.ac.uk>
2019-06-28 08:44:40 +0100

    debug conda build

--- deploy_anaconda.sh
+++ deploy_anaconda.sh
@@ -3,6 +3,10 @@
 
 set -e
 
+echo "print dir content"
+ls
+ls dist
+
 echo "Build noarch package..."
 conda build meta.yaml --python 3.6 --numpy 1.1 --output-folder conda_build --debug
 
--- pycoQC/__init__.py
+++ pycoQC/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 
-__version__ = '2.3.0.9'
+__version__ = '2.3.0.10'
 __all__ = ["pycoQC", "Fast5_to_seq_summary", "common"]
