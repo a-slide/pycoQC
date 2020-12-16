c509c6a1708681cdc339aa7cc51c06442d58f0fe
a-slide <aleg@ebi.ac.uk>
2020-12-16 13:21:26 +0000

    Update dependencies and bump up version

--- pycoQC/__init__.py
+++ pycoQC/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 
-__version__ = '2.5.0.23'
+__version__ = '2.5.1'
 __all__ = ["pycoQC", "Fast5_to_seq_summary", "Barcode_split", "common"]
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 # Define package info
 name = "pycoQC"
-version = "2.5.0.23"
+version = "2.5.1"
 description = "PycoQC computes metrics and generates interactive QC plots for Oxford Nanopore technologies sequencing data"
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -28,14 +28,14 @@ setup(
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python :: 3'],
     install_requires = [
-        'numpy==1.17.1',
-        'scipy==1.3.1',
-        'pandas==0.25.1',
+        'numpy>=1.19',
+        'scipy>=1.5',
+        'pandas>=1.1',
         'plotly==4.1.0',
-        'jinja2==2.10.1',
-        'h5py==2.9.0',
-        'tqdm==4.35.0',
-        'pysam==0.15.3'],
+        'jinja2>=2.10',
+        'h5py>=3.1',
+        'tqdm>=4.54',
+        'pysam>=0.16'],
     packages = [name],
     package_dir = {name: name},
     package_data = {name: ['templates/*']},
